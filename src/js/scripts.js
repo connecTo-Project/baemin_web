771f61c349f003eaa7201e441bf0c0ac56d8c5ac
loco9939 <kls9939@naver.com>
2022-08-04 10:44:48 +0900

    feat: Create carousel
    캐러셀 기능 구현

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -22,3 +22,27 @@ $(document).ready(function () {
     }
   }, 3000);
 });
+// 메뉴페이지 이미지 슬라이드
+$(document).ready(function () {
+  var num = 0;
+
+  setInterval(function () {
+    if (num < 2) {
+      num++;
+      $(".ad .ad__list").animate(
+        {
+          left: "-=390",
+        },
+        "slow"
+      );
+    } else {
+      num = 0;
+      $(".ad .ad__list").animate(
+        {
+          left: 0,
+        },
+        "slow"
+      );
+    }
+  }, 3000);
+});
